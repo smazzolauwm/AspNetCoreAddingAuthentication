ac2ad13739d93b5730e1d81de8df5257701de5ea
samantha mazzola <smazzola@uwm.com>
2021-09-23 14:24:45 -0400

    create account controller adding fields and a constructor

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        // fields : data members 
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager) 
+        {
+            this._userManager = _userManager;
+            this._signInManager = _signInManager;
+        }
+
+        public static void Account()
+        { 
+            // this is empty 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
